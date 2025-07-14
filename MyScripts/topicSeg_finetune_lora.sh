9df8821946df2c6a5c4287f53d7ac0a49dda0201
SeanCheng1996 <sc159@rice.edu>
2025-07-14 16:12:47 -0500

    add code for topic_seg_finetune_lora. training scripts and dataloaders.

--- MyScripts/topicSeg_finetune_lora.sh
+++ MyScripts/topicSeg_finetune_lora.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-# Uncomment and set the following variables correspondingly to run this script:
+# Set the following variables correspondingly to run this script:
+
+################## VICUNA ##################
+PROMPT_VERSION=v1
 
 model_base=lmsys/vicuna-7b-v1.5
-output_dir="${1:-./checkpoints_topic_seg}"
+output_dir="${1:-./checkpoints_topic_seg_a100}"
 
+# PROJECTOR="/PATH_TO/mm_projector.bin" # generated using pretrain.sh
+PROJECTOR="/data/sc159/LLaVARad/checkpoints/topic_seg/llava_biomedclip_cxr_518-pt-1e-1e-3-20250710031958/mm_projector.bin" # todo, sirius
+#PROJECTOR="/work/devika/sc159/llava_rad/LLaVA-Rad-main/checkpoints_topic_seg_a100/llava_biomedclip_cxr_518-pt-1e-1e-3-20250710031958/mm_projector.bin" # todo, rapid
+vision_tower="biomedclip_cxr_518"
+vision_tower_config="llava/model/multimodal_encoder/open_clip_encoder/model_configs/biomedclip_cxr_518.json"
+vision_tower_checkpoint="biomedclipcxr_518_checkpoint.pt"
+################## VICUNA ##################
+
+
+################## Data ##################
 # data_path=/PATH_TO/physionet.org/files/llava-rad-mimic-cxr-annotation/1.0.0/chat_train_MIMIC_CXR_all_gpt4extract_rulebased_v1.json
 data_path=/data/sc159/data/MIMIC_III/llava_rad_topic/chat_train_MIMIC_CXR_all_gpt4extract_rulebased_v3.json # todo, sirius
 #data_path=/work/devika/data/MIMIC_III/MIMIC_III/llava_rad_topic/chat_train_MIMIC_CXR_all_gpt4extract_rulebased_v3.json #todo, rapid
@@ -12,44 +25,39 @@ data_path=/data/sc159/data/MIMIC_III/llava_rad_topic/chat_train_MIMIC_CXR_all_gp
 mask_path=/data/sc159/data/MIMIC_III/segmentation_single # todo, sirius
 #mask_path=/work/devika/data/MIMIC_III/MIMIC_III/segmentation_single # todo, rapid
 
-loader="mimic_topic_findings"
+loader="mimic_topic_reason_findings"
 
 # image_folder=/PATH_TO/physionet.org/files/mimic-cxr-jpg/2.0.0/files
 image_folder=/data/sc159/data/MIMIC_III/physionet.org/files/mimic-cxr-jpg/2.0.0/files # todo, sirius
 #image_folder=/work/devika/data/MIMIC_III/MIMIC_III/physionet.org/files/mimic-cxr-jpg/2.0.0/files # todo, rapid
-
+################## Data ##################
 
 ################## Run name ##################
-vision_tower="biomedclip_cxr_518"
-vision_tower_config="llava/model/multimodal_encoder/open_clip_encoder/model_configs/biomedclip_cxr_518.json"
-vision_tower_checkpoint="biomedclipcxr_518_checkpoint.pt" 
-
-epoch="${2:-1}"
-bsz="${3:-16}"
-grad_acc="${4:-8}" # todo, sirius
-#grad_acc="${4:-4}" # todo, rapid
-lr="1e-3"
-schedule="pt-${epoch}e"
-run_name="${vision_tower}-${schedule}-${lr}-$(date +%Y%m%d%H%M%S)"
+epoch="${2:-3}"
+bsz="${3:-8}"
+lr="1e-4"
+schedule="lora-${epoch}e"
+export run_name="${vision_tower}-${schedule}-${lr}-$(date +%Y%m%d%H%M%S)"
 echo $run_name > run_name
 ################## Run name ##################
 
-# Global batch size should be 256
 
-WANDB_RUN_ID="llava-pt-$(date +%Y%m%d%H%M%S)" WANDB_PROJECT="llava_sirius_topic_seg" WANDB_RUN_GROUP=pre-train \
+# Batch size is set for 4-GPU machines, 64 in total.
+WANDB_PROJECT="llava_topic_seg" WANDB_RUN_ID="llava-ft-$(date +%Y%m%d%H%M%S)" WANDB_RUN_GROUP=fine-tune \
     deepspeed llava/train/train_mem_forTopicSeg.py \
-    --deepspeed ./MyScripts/zero2.json \
+    --deepspeed ./scripts/zero2.json \
+    --lora_enable True \
+    --lora_alpha 128 \
     --model_name_or_path ${model_base} \
-    --version v1 \
+    --version $PROMPT_VERSION \
     --data_path ${data_path} \
-    --mask_path ${mask_path} \
     --loader ${loader} \
     --image_folder ${image_folder} \
     --vision_tower ${vision_tower} \
     --vision_tower_config ${vision_tower_config} \
     --vision_tower_checkpoint ${vision_tower_checkpoint} \
+    --pretrain_mm_mlp_adapter ${PROJECTOR} \
     --mm_projector_type mlp2x_gelu \
-    --tune_mm_mlp_adapter True \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
@@ -58,11 +66,11 @@ WANDB_RUN_ID="llava-pt-$(date +%Y%m%d%H%M%S)" WANDB_PROJECT="llava_sirius_topic_
     --num_train_epochs ${epoch} \
     --per_device_train_batch_size ${bsz} \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps ${grad_acc} \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 24000 \
-    --save_total_limit 1 \
+    --save_steps 800 \
+    --save_total_limit 2 \
     --learning_rate ${lr} \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
@@ -71,7 +79,7 @@ WANDB_RUN_ID="llava-pt-$(date +%Y%m%d%H%M%S)" WANDB_PROJECT="llava_sirius_topic_
     --tf32 True \
     --model_max_length 4096 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
     --lazy_preprocess True \
+    --dataloader_num_workers 4 \
     --report_to wandb \
     --run_name ${run_name}
--- llava/utils.py
+++ llava/utils.py
@@ -221,6 +221,39 @@ def data_loader_mimic_topic_findings(data_path, split):
     logging.info(f"loaded {len(ret)}/{len(dataset)} samples. from utils")
     return ret
 
+def data_loader_mimic_topic_reason_findings(data_path, split):
+    logging.info(f"using the MIMIC-CXR topic finding loader: MIMIC {split}.")
+    with open(data_path) as f:
+        dataset = json.load(f)
+    with open(BAD_IMAGE_PATHS, 'r') as f:
+        bad_image_paths = set(line.strip() for line in f)
+    ret = []
+    for d in dataset:
+        # Skip empty topic findings
+        if not isinstance(d["conversations"][2]["value"], str):
+            continue
+        if d['view'] not in ('AP', 'PA'):
+            continue
+        if d['image'] in bad_image_paths:
+            print(f"ignoring {d['image']}")
+            continue
+        if d['image'].startswith("mimic/"):
+            d['image'] = d['image'][len('mimic/'):]
+        if d['reason'] is not None:
+            reason = d['reason'].replace('\n', ' ')
+            d['conversations'][0][
+                'value'] = f"Given the image <image>\nand the segmented part <image>\nand the following indication: {reason}\nProvide a description of the findings in the radiology image for the following organs or diseases: {d['topic']}."
+        else:
+            d['conversations'][0][
+                'value'] = f"Given the image <image>\nand the segmented part <image>\nProvide a description of the findings in the radiology image for the following organs or diseases: {d['topic']}."
+        # move "tpoic_based" under "gpt" to align with other code, then delete "topic_based"
+        d['conversations'][1]['value'] = d['conversations'][2]['value']
+        d['conversations'] = d['conversations'][:2]
+
+        ret.append(d)
+    logging.info(f"loaded {len(ret)}/{len(dataset)} samples. from utils")
+    return ret
+
 
 def data_loader_IUXRay_findings(data_path, split):
     logging.info(f"using the IUXray loader: IUXRay {split}.")
@@ -246,4 +279,5 @@ data_loaders = {
     "mimic_cxr_all_views_findings": data_loader_mimic_cxr_all_views_findings,
     "iuxray_test_findings": lambda x: data_loader_IUXRay_findings(x, "test"),
     "mimic_topic_findings": lambda x: data_loader_mimic_topic_findings(x, "train"),
+    "mimic_topic_reason_findings": lambda x: data_loader_mimic_topic_reason_findings(x, "train"),
 }
