f0ed26e43563706f85ca9d65bbb573e769d31c99
SeanCheng1996 <sc159@rice.edu>
2025-07-26 22:22:52 -0500

    add code to clean repeated topics in topic_seg_report jsonl file

--- MyData/ReportProcess/_11_compare_ori_mine.py
+++ MyData/ReportProcess/_11_compare_ori_mine.py
@@ -1,7 +1,8 @@
 import json
-
+from tqdm import tqdm
 from _6_read_result_file import extract_response_file
-
+import matplotlib.pyplot as plt
+from collections import Counter
 
 def extractReport(file_path):
     """
@@ -44,8 +45,9 @@ def extractReport(file_path):
 if __name__ == '__main__':
         split = "valid"
         processed_id_report_json = extractReport(
-                "/data/sc159/data/MIMIC_III/llava_rad_topic/chat_valid_MIMIC_CXR_all_gpt4extract_rulebased_v3.json")
+                "/data/sc159/data/MIMIC_III/llava_rad_topic/chat_test_MIMIC_CXR_all_gpt4extract_rulebased_v4.json")
 
+        # show llavarad and my report side by side
         for id in list(processed_id_report_json.keys())[:10]:
             print("="*60)
             print(f"id:{id}")
@@ -55,3 +57,37 @@ if __name__ == '__main__':
             for topic, topic_sents in zip(processed_id_report_json[id]['topics'], processed_id_report_json[id]['topic_sents']):
                 print(f"  {topic}")
                 print(f"    -{topic_sents}")
+
+        # stats how many repeated topics
+        diff_cnts=[]
+        for id in tqdm(list(processed_id_report_json.keys())):
+            topics_list=processed_id_report_json[id]['topics']
+            diff=len(topics_list)-len(set(topics_list))
+            if diff>0:
+                diff_cnts.append(diff)
+                print("="*60)
+                for i in range(len(topics_list)):
+                    print(f"{topics_list[i]}:     {processed_id_report_json[id]['topic_sents'][i]}")
+
+        counts = Counter(diff_cnts)
+        numbers = list(counts.keys())
+        frequencies = list(counts.values())
+
+        plt.bar(numbers, frequencies, color='skyblue')
+
+        plt.title(f'Redundant topics frequency. {len(diff_cnts)}')
+        plt.xlabel('#Redundant topics')
+        plt.ylabel('Frequency')
+        plt.show()
+
+        # # generate a jsonl in the format of prediction, so that the evaluate code can be applied on the processed reports.
+        # with open("/data/sc159/data/MIMIC_III/llava_rad_topic/evaluate/llavarad_vs_topic.jsonl", 'w', encoding='utf-8') as f_out:
+        #     for id in tqdm(list(processed_id_report_json.keys())):
+        #         ori_report = processed_id_report_json[id]['ori']
+        #         topic_report=" ".join(processed_id_report_json[id]['topic_sents'])
+        #         out_entry = {
+        #             "id": id,
+        #             "reference": ori_report,
+        #             "prediction": topic_report
+        #         }
+        #         f_out.write(json.dumps(out_entry, ensure_ascii=False) + "\n")
---
+++ MyData/ReportProcess/_9_2_clean_repeated_topics.py
@@ -0,0 +1,79 @@
+import json
+from tqdm import tqdm
+from _6_read_result_file import extract_response_file
+import matplotlib.pyplot as plt
+from collections import Counter
+
+
+def extractReport(file_path):
+    """
+    file_path: llavaRad json file
+
+    return:
+    id_report_json:
+    {
+        unique report id :
+            {
+            image: "",
+            ori:"",
+            topics: ["","",...],
+            topic_sents: ["", "",...]
+            }
+    }
+    """
+
+    id_report_json = {}
+    with open(file_path, 'r') as f:
+        reports = json.load(f)
+
+    for report in reports:
+        cur_id = report['id']
+
+        if cur_id not in id_report_json:
+            id_report_json[cur_id] = {"image": report['image'], "ori": report['conversations'][1]['value']}
+
+        if "topics" not in id_report_json[cur_id]:
+            id_report_json[cur_id]['topics'] = []
+        id_report_json[cur_id]['topics'].append(report['topic'])
+
+        if "topic_sents" not in id_report_json[cur_id]:
+            id_report_json[cur_id]['topic_sents'] = []
+        id_report_json[cur_id]['topic_sents'].append(report['conversations'][2]['value'])
+
+    return id_report_json
+
+
+if __name__ == '__main__':
+    # file_path = "/data/sc159/data/MIMIC_III/llava_rad_topic/chat_test_MIMIC_CXR_all_gpt4extract_rulebased_v3.json"
+    # save_path = "/data/sc159/data/MIMIC_III/llava_rad_topic/chat_test_MIMIC_CXR_all_gpt4extract_rulebased_v4.json"
+    #
+    # # stats how many repeated topics
+    # res = []
+    # visited_id_topics_json = {}  # {id: set(topics), ...}
+    # with open(file_path, 'r') as f:
+    #     ori_res = json.load(f)
+    #     print(f"previous len: {len(ori_res)}")
+    #
+    #     for report in ori_res:
+    #         id = report['id']
+    #         topic = report['topic']
+    #         if id in visited_id_topics_json and topic in visited_id_topics_json[id]:  # repeated
+    #             continue
+    #         else:  # new
+    #             if id not in visited_id_topics_json:
+    #                 visited_id_topics_json[id] = set()
+    #             visited_id_topics_json[id].add(topic)
+    #             res.append(report)
+    #
+    #
+    # print(f"current len: {len(res)}")
+    # with open(save_path, 'w') as f:
+    #     json.dump(res, f, indent=2)
+
+    ids=set()
+    with open("/data/sc159/data/MIMIC_III/llava_rad/chat_test_MIMIC_CXR_all_gpt4extract_rulebased_v1.json", 'r') as f:
+        res=json.load(f)
+        for report in res:
+            if report['view'] in ["AP", "PA"] and report['generate_method']=='gpt4':
+                ids.add(report['image'])
+        print(len(ids))
